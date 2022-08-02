39ae9d2e8a6f9e15163dd706c4adaaad0e77a706
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-02 11:20:09 +0300

    Add default to expense amount

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_081120) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_081858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "expenses", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount"
+    t.decimal "amount", default: "0.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
