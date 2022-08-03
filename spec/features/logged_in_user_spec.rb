a8d204d7d9181c53303f30a9658dd9f283bbea21
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-03 12:40:31 +0300

    Add spec model helper, add signup and login spec

---
+++ spec/features/logged_in_user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+require 'helpers/model_helper'
+
+RSpec.feature 'Login Page' do
+  feature 'shows login' do
+    background do
+      visit new_user_session_path
+      @user1 = User.create(name: 'Ben', email: 'ben@gmail.com', password: '123456')
+      within 'form' do
+        fill_in 'Email', with: @user1.email
+        fill_in 'Password', with: @user1.password
+      end
+      click_button 'Log in'
+      @groups = create_groups_for_user(@user1).first
+      @expenses = create_expenses_for_user(@user1).first
+    end
+
+    scenario 'shows welcome greeting' do
+      expect(page).to have_content('Welcome to the Rails Mint')
+    end
+
+    scenario 'shows sign out link after user login' do
+      expect(page).to have_content('Sign out')
+    end
+  end
+end
---
+++ spec/helpers/model_helper.rb
@@ -0,0 +1,29 @@
+def create_and_activate_user(name)
+  user = User.create!(name:, email: 'ben@gmail.com', password: 'password')
+  user.save!
+  user
+end
+
+def create_groups_for_user(user, count: 1)
+  groups = []
+  count.times do |i|
+    groups << Group.create!(
+      user:,
+      name: "Group #{i}",
+      icon: "https://image.com",
+    )
+  end
+  groups
+end
+
+def create_expenses_for_user(user, count: 1)
+  expenses = []
+  count.times do |i|
+    expenses << Expense.create!(
+      author: user,
+      name: "Expense #{i}",
+      amount: 10
+    )
+  end
+  expenses
+end
