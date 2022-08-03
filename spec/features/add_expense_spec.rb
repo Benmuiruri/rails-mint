a978a9d7e52dd98825de0072ef7bb71d26d35447
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-03 14:12:38 +0300

    fix rubocob offenses

--- spec/features/add_expense_spec.rb
+++ spec/features/add_expense_spec.rb
@@ -14,20 +14,13 @@ RSpec.feature 'Login Page' do
       @groups = create_groups_for_user(@user1).first
       @expenses = create_expenses_for_user(@user1).first
     end
-
-    scenario 'shows welcome greeting if user has no groups' do
-      expect(page).to have_content('Welcome to the Rails Mint')
-    end
-
     scenario 'Add a category' do
       visit new_group_path
       fill_in 'Name', with: 'Bills'
       fill_in 'Icon', with: 'https://image.com'
       click_button 'Add Category'
-
       expect(page).to have_content('You have tracked $10.0 across 2 categories')
     end
-
     scenario 'Add a expense in the Bills category' do
       visit group_path(Group.last)
       click_link 'Add a new Expense'
@@ -36,7 +29,6 @@ RSpec.feature 'Login Page' do
       select('Group 0', from: 'expense[group_id]')
       click_button 'Add Expense'
       expect(page).to have_content('You have tracked $220.0 of Group 0')
-      expect(page).to have_current_path(group_path(Group.last))
     end
   end
 end
